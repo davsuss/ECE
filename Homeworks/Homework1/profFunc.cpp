1fe50330f521121a9526ac97e6823f00470c921d
David Sussman <davsuss@vt.edu>
2013-09-04 22:42:26 -0400

    Fixed Testing Code Going to Submit this as a base case, Will add more cases before submitting

--- Homeworks/Homework1/profFunc.cpp
+++ Homeworks/Homework1/profFunc.cpp
@@ -1,2 +1,18 @@
 #include "profFunc.h"
+#include <QDebug>
+int profFunc(int sourceStringLen, const char *sourceString,int destStringLen, char *destString)
+{
+    if(sourceString == 0 || destString == 0 || sourceStringLen == 0 || destStringLen == 0)
+        return 0;
+    int result = 0;
+    for(int x = 0; x < sourceStringLen; x++)
+    {
 
+        if(x > destStringLen)
+            break;
+
+      *(destString+x) = *(sourceString+x);
+      result++;
+    }
+    return result;
+}
--- Homeworks/Homework1/testproffunc.cpp
+++ Homeworks/Homework1/testproffunc.cpp
@@ -1,30 +1,58 @@
 #include <QtTest/QtTest>
-
+#include "profFunc.h"
+#include <string>
+#include <QDebug>
+using namespace std;
+//int profFunc(int sourceStringLen, const char *sourceString,int destStringLen, char *destString);
 class TestprofFunc: public QObject
 {
     Q_OBJECT
 
 private slots:
-    void toUpper_data();
-    void toUpper();
+    void testProfFunc_data();
+    void testProfFunc();
 };
 
-void TestprofFunc::toUpper_data()
+void TestprofFunc::testProfFunc_data()
 {
-    QTest::addColumn<QString>("string");
-    QTest::addColumn<QString>("result");
+    //Function Inputs
+    QTest::addColumn<int>("sourceStringLen");
+    QTest::addColumn<QString>("sourceString");
+    QTest::addColumn<int>("destStringLen");
+    QTest::addColumn<QString>("destString");
 
-    QTest::newRow("all lower") << "hello" << "HELLO";
-    QTest::newRow("mixed")     << "Hello" << "HELLO";
-    QTest::newRow("all upper") << "HELLO" << "HELLO";
+    //Expected Results
+    QTest::addColumn<QString>("resultDestString");
+    QTest::addColumn<int>("expectedResult");
+
+    QTest::newRow("Test 1") << 10 << "Virginia Tech" << 10 << "b         " <<  "Virginia T" << 10;
+    QTest::newRow("Test 2") << 0 << QString() << 10 << "b         " << "b         " << 0;
+    QTest::newRow("Test 3") << 10 << "Virginia Tech" << 0 <<QString() << QString() << 0;
 }
 
-void TestprofFunc::toUpper()
+void TestprofFunc::testProfFunc()
 {
-    QFETCH(QString, string);
-    QFETCH(QString, result);
+    QFETCH(int, sourceStringLen);
+    QFETCH(QString, sourceString);
+    QFETCH(int, destStringLen);
+    QFETCH(QString, destString);
 
-    QCOMPARE(string.toUpper(), result);
+
+    QFETCH(QString, resultDestString);
+    QFETCH(int, expectedResult);
+
+     string sourceCString = sourceString.toStdString();
+    const char *source = sourceCString.c_str();
+
+    char* dest;
+    string destCString = destString.toStdString();
+    dest = new char [destCString.size()+1];
+    strcpy( dest, destCString.c_str() );
+
+
+    int result = profFunc(sourceStringLen,source,destStringLen,dest);
+    QCOMPARE(result,expectedResult);
+    QCOMPARE(QString::fromUtf8(dest),resultDestString);
 }
 
 QTEST_MAIN(TestprofFunc)
